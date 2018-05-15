870f39642f139970af852b6e5598d2ef0533aa02
Damiaan Peeters <damiaan@umbrace.be>
2018-05-15 18:25:09 +0200

    Add random person surfacecontroller

---
+++ Website.Sample/ExternalData/RandomPersonController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using umbraco;
+using Umbraco.Web.Mvc;
+using Website.Sample.DbModels;
+
+namespace Website.Sample.ExternalData
+{
+    public class RandomPersonController : SurfaceController
+    {
+        public ActionResult Index()
+        {
+            var db = ApplicationContext.DatabaseContext.Database;
+            // demo code, do not do this in production
+            var randomperson = db.Fetch<Person>("WHERE Id>0").RandomOrder().First();
+            return View("Index", randomperson);
+        }
+    }
+}

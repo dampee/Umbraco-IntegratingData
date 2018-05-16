c02e6ff9a4265331de4c267da5b6d019eacdc986
Damiaan Peeters <damiaan@umbrace.be>
2018-05-16 18:13:42 +0200

    Import dashboard (wip)

---
+++ Website.Sample/App_Plugins/ImportDashBoard/index.controller.js
@@ -0,0 +1,9 @@
+﻿angular.module("umbraco").controller("ImportDashboardController", function ($scope,userService) {
+    var vm = this;
+    var user = userService.getCurrentUser().then(function (user) {
+        console.log(user);
+        vm.UserName = user.name;
+        vm.AllowImports = user.userType === "admin";
+    });
+    console.log(vm);
+});
---
+++ Website.Sample/ImportData/ImportController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Website.Sample.ImportData
+{
+    public class ImportController : UmbracoAuthorizedController
+    {
+        private readonly IImportService _importService;
+
+        public ImportController(IImportService importService)
+        {
+            _importService = importService;
+        }
+
+        [System.Web.Mvc.HttpPost]
+        [System.Web.Mvc.AcceptVerbs("POST")]
+        public string ProcessImportData(string immediate, [Bind(Include = "File")] HttpPostedFileBase file)
+        {
+            var temp = Request;
+            if (immediate != "on")
+            {
+                // send to hangfire
+                throw new NotImplementedException();
+                return "Import scheduled";
+            }
+
+            var sb = new StringBuilder();
+            using (var stream = new MemoryStream())
+            {
+                file.InputStream.CopyTo(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+                if (_importService.ProcessDataImportFromStream(stream, file.FileName, sb))
+                {
+                    //todo Export SB result
+                    return sb.ToString();
+                };
+            }
+
+            return "Error: " + sb;
+        }
+
+        [System.Web.Mvc.HttpGet]
+        public bool Ping()
+        {
+            return true;
+        }
+
+    }
+}
